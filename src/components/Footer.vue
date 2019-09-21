9b9f11b49699563f031902665938144faff2c1a1
pailin-h3 <jinjuta.l@st.econ.tu.ac.th>
2019-09-21 20:03:37 +0700

    Homework 06 submission

--- src/components/Footer.vue
+++ src/components/Footer.vue
@@ -1,20 +1,29 @@
 <template>
-    <div class="d-flex align-items-center">
-        <p>{{info.editorName}}</p><br>
-        <!-- <p>{{info.date}}</p> -->
+    <div>
+        <p class="footerp">{{info.editorName}}</p>
+        <p class="footerp">{{info.date}}/{{info.month}}/{{info.year}}</p>
     </div>
 </template>
 <script>
 export default {
     // props: ['editorName', 'date', 'month', 'year'],
     data: function(){
-        // var d = new date();
+         var d = new Date();
       return {
           info : {
               editorName: "Jinjuta Limwattana",
-            //   date: d.getDate(),
+               date: d.getDate(),
+               month: d.getMonth(),
+               year: d.getFullYear()
           }
       }
     }
 }
-</script>>
+</script>
+
+<style>
+.footerp {
+    text-align: center;
+    line-height: 0.5;
+}
+</style>
